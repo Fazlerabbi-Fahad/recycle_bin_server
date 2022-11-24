2fd1d4c44be4a54884073887664d6d8f4bf510d1
Fazle Rabbi Fahad <fazlerabbifahad.54@gmail.com>
2022-11-25 03:14:28 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 //middleware
@@ -8,6 +10,17 @@ app.use(cors());
 app.use(express.json());
 
 const categories = require('./Data/categories.json')
+
+//connect mongodb
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.1bviphv.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
 
 app.get('/categories', (req, res) => {
     res.send(categories);
