ffaf0636f7fc5c33aa56590f94228a9f4286768b
Fazle Rabbi Fahad <fazlerabbifahad.54@gmail.com>
2022-11-25 03:34:07 +0600

    mongodb connected successfully

--- index.js
+++ index.js
@@ -4,33 +4,43 @@ const app = express();
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
+require('dotenv').config()
+
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-const categories = require('./Data/categories.json')
-
 //connect mongodb
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.1bviphv.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
 
 
-app.get('/categories', (req, res) => {
-    res.send(categories);
+async function run() {
+    const categoriesCollection = client.db('recycleBIN').collection('categories');
 
-})
+    try {
+        app.get('/categories', async (req, res) => {
+            const query = {};
+            const results = await categoriesCollection.find(query).toArray();
+            res.send(results)
+        })
+
+    }
+    finally {
+
+    }
+
+}
+
+run().catch(err => console.log(err));
+
 
 app.get('/', (req, res) => {
     res.send(`Recycle Bin server is running`)
 });
 
-app.listen(port, (req, res) => {
+app.listen(port, () => {
     console.log(`Recycle Bin is running on ${port}`);
 })
